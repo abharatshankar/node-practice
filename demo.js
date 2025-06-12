0751ef4262976b2d4d66b3bfcdec848ac3d074f5
abharatshankar <bharatshankar5@gmail.com>
2025-06-12 12:04:33 +0530

    day 1- require , module.export learned

---
+++ demo.js
@@ -0,0 +1,10 @@
+const registerUser = require('./registration');
+
+function greetSai(){
+    console.log("Jai Sai Ram")
+}
+
+greetSai();
+
+registerUser.checkingUser();
+registerUser.registerUser();
---
+++ registration.js
@@ -0,0 +1,12 @@
+function checkingUser(){
+    console.log("checking user exists or not");
+}
+
+function registerUser(){
+    console.log("user does't exist so new registration");
+}
+
+module.exports={
+    checkingUser:checkingUser,
+    registerUser:registerUser
+}
